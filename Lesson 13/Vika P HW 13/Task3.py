f20133f92fab730ff2669e6a79daef695b2c55bd
Vika P <vi.priadko@gmail.com>
2022-12-03 13:30:30 +0100

    Yayyyy!

---
+++ Lesson 13/Vika P HW 13/Task3.py
@@ -0,0 +1,6 @@
+"""
+Write a function called `choose_func` which takes a list of nums and 2 callback functions.
+If all nums inside the list are positive, execute the first function on that list and return the result of it.
+Otherwise, return the result of the second one
+"""
+
