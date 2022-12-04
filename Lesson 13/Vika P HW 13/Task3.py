389053031d9dcac9a2810b1caeeb8df7d6d6ab6e
Vika P <vi.priadko@gmail.com>
2022-12-04 18:05:29 +0100

    Version!

--- Lesson 13/Vika P HW 13/Task3.py
+++ Lesson 13/Vika P HW 13/Task3.py
@@ -4,3 +4,33 @@ If all nums inside the list are positive, execute the first function on that lis
 Otherwise, return the result of the second one
 """
 
+
+def func1(nums):
+    print ([num ** 2 for num in nums])
+
+def func2(nums):
+    print ([num for num in nums if num > 0])
+
+
+def choose_func(nums: list, func1, func2):
+    count = 0
+    for i in nums:
+        if i < 0:
+            count += 1
+    if count == 0:
+        func1(nums)
+    if count >= 1:
+        func2(nums)
+
+
+
+
+nums = [1, -2, 3, -4, 5]
+# nums = [1, 2, 3, 4, 5]
+choose_func(nums, func1, func2)
+
+
+
+
+
+
